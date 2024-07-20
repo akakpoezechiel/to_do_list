6aa7c9a359fed3024219ca2ef144d97f48953e82
AKAKPO Ezechiel <ezechielakakpo3@gmail.com>
2024-07-20 12:42:20 +0000

    first commit

---
+++ app/src/main/java/com/example/todolist/data/room/repositories/OTPCodeRepository.kt
@@ -0,0 +1,13 @@
+package com.example.todolist.data.room.repositories
+
+import android.provider.ContactsContract.CommonDataKinds.Email
+import com.example.todolist.data.room.daos.OTPCodeDao
+import com.example.todolist.data.room.models.OTPCode
+import kotlinx.coroutines.flow.Flow
+
+class OTPCodeRepository(
+    private val otpCodeDao: OTPCodeDao
+) {
+    suspend fun insert(otpCode: OTPCode){otpCodeDao.insert(otpCode)}
+    fun checkOtpCode(email: String, code: String): Flow<List<OTPCode>> = otpCodeDao.checkOtpCode(email,code)
+}
